4c6e558f58846beae7acabd1f7d143a05e41cd3f
Dick Kluis <dick@morriscs.com>
2024-11-07 14:07:25 -0500

    Add Collected Data Grid Page and Update Navigation
    Added a new `CollectedDataGrid.razor` component to display API data with filtering, sorting, and search functionalities. Updated `NavMenu.razor` to include a link to the new data grid page, improving overall navigation. Additionally, removed sensitive information from connection strings in the `TycherosmonitoringContext.cs`.

--- Models/TycherosmonitoringContext.cs
+++ Models/TycherosmonitoringContext.cs
@@ -18,10 +18,6 @@ public partial class TycherosmonitoringContext : DbContext
 
     public virtual DbSet<Collectedapidatum> Collectedapidata { get; set; }
 
-//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//         => optionsBuilder.UseMySql("server=localhost;user id=dick;password=Sandy2836;database=tycherosmonitoring", Microsoft.EntityFrameworkCore.ServerVersion.Parse("11.5.2-mariadb"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
